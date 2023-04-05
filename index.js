9e125b036d31cb8ac0020761bbe48f1b2d7281c5
Mutindi Hope <msndindi@outlook.com>
2023-04-05 23:53:16 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// code your solution here
+function superbowlWin(records) {
+    const winningRecord = records.find(record => record.result === "W");
+    return winningRecord ? winningRecord.year : undefined;
+  }
+  const records = [
+    { year: 2022, result: 'L' },
+    { year: 2021, result: 'W' },
+    { year: 2020, result: 'L' },
+    { year: 2019, result: 'L' },
+    { year: 2018, result: 'W' }
+  ];
+  
+  console.log(superbowlWin(records)); // Output: 2021
+  
